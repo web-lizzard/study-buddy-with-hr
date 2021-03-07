372d2ee5391463f58d80f4047395c253fa2358e9
web-lizzard <czarnous.adrian@gmail.com>
2021-03-07 17:12:15 +0100

    create form add student

--- src/components/organisms/UsersList/UsersList.tsx
+++ src/components/organisms/UsersList/UsersList.tsx
@@ -1,69 +1,60 @@
-import React, { FC, useState, useEffect } from 'react';
-
-import { mockAPI } from 'utils/mockApi';
+import React, { FC } from 'react';
 import { UsersListItem } from 'components/molecules/UsersListItem/UserListItem';
 import { Wrapper } from './UsersList.styles';
 import { UserInterface } from '../../../interfaces/users.interface';
-import { FormField } from '../../molecules/FormField/FormField';
-import { StyledTitle } from '../../molecules/UsersListItem/UserListItem.styles';
-import { Button } from '../../atoms/Button/Button';
 
-interface formValueInterface {
-  name: string;
-  attendance: string;
-  average: string;
+import { StyledTitle } from '../../molecules/UsersListItem/UserListItem.styles';
+
+interface Props {
+  deleteUser: (name: string) => void;
+  users: UserInterface[];
+  isLoading: boolean;
 }
 
-const UsersList: FC = () => {
-  const [users, setUsersState] = useState<UserInterface[]>([]);
-  const [isLoading, setIsLoadingState] = useState<boolean>(true);
-  const [formValues, setFormValueState] = useState<formValueInterface>({
-    name: '',
-    attendance: '',
-    average: '',
-  });
+const UsersList: FC<Props> = (props: Props) => {
+  // const [users, setUsersState] = useState<UserInterface[]>([]);
+  // const [isLoading, setIsLoadingState] = useState<boolean>(true);
+  // const [formValues, setFormValueState] = useState<formValueInterface>({
+  //   name: '',
+  //   attendance: '',
+  //   average: '',
+  // });
 
-  const handleFormFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormValueState({ ...formValues, [e.target.name]: e.target.value });
-  };
+  // const handleFormFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFormValueState({ ...formValues, [e.target.name]: e.target.value });
+  // };
 
-  const handleAddStudent = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const newStudent = { ...formValues };
-    setUsersState([...users, newStudent]);
-    setFormValueState({
-      name: '',
-      attendance: '',
-      average: '',
-    });
-  };
+  // const handleAddStudent = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const newStudent = { ...formValues };
+  //   setUsersState([...users, newStudent]);
+  //   setFormValueState({
+  //     name: '',
+  //     attendance: '',
+  //     average: '',
+  //   });
+  // };
 
-  const fetchData = async () => {
-    const data = await mockAPI();
-    if (data) {
-      setIsLoadingState(false);
-      setUsersState(data);
-    }
-  };
+  // const fetchData = async () => {
+  //   const data = await mockAPI();
+  //   if (data) {
+  //     setIsLoadingState(false);
+  //     setUsersState(data);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  const deleteUser = (name: string) => {
-    const filterData = users.filter((user) => user.name !== name);
-    setUsersState(filterData);
-  };
+  // const deleteUser = (name: string) => {
+  //   const filterData = users.filter((user) => user.name !== name);
+  //   setUsersState(filterData);
+  // };
+  const { deleteUser, isLoading, users } = props;
 
   return (
     <>
-      <Wrapper as="form" onSubmit={handleAddStudent}>
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField label="Name" name="name" id="name" value={formValues.name} onChange={handleFormFieldChange} />
-        <FormField label="Attendance" name="attendance" id="attendance" value={formValues.attendance} onChange={handleFormFieldChange} />
-        <FormField label="Average" name="average" id="average" value={formValues.average} onChange={handleFormFieldChange} />
-        <Button type="submit">Add</Button>
-      </Wrapper>
       <Wrapper>
         <StyledTitle>{isLoading ? 'Loading...' : 'Students List'}</StyledTitle>
         <ul>
---
+++ src/interfaces/formValue.interface.ts
@@ -0,0 +1,5 @@
+export interface formValueInterface {
+  name: string;
+  attendance: string;
+  average: string;
+}
